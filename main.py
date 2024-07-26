3a6b430e52e8d2eaa47f4ad113420c1204a3c857
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2024-07-26 21:25:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 
+from morse_talk import *
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def main():
+    st.set_page_config(page_title="Code Converter", page_icon="./-")
+    with st.sidebar:
+        choice=option_menu(menu_title="Code Converter",options=["Morse Code","Binary Code"])
+
+        if choice=="Morse Code":
+            pass
+            
+
+        elif choice=="Binary Code":
+            pass
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
+    
